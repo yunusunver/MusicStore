0ebea0da13d75f9905e127525c0b1472c92c571c
yunusunver <yunus.unver1995@gmail.com>
2018-07-08 15:05:00 +0300

    Music Store

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDb = new MusicStoreEntities();
+        // GET: Store
+        public ActionResult Index()
+        {
+            var genreList = storeDb.Genres.ToList();
+            return View(genreList);
+        }
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = storeDb.Genres.Include("Albums").Single(x=>x.Name==genre);
+            return View(genreModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var album = storeDb.Albums.Find(id);
+            return View(album);
+        }
+    }
+}
---
+++ MusicStore/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MusicStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            System.Data.Entity.Database.SetInitializer(
+            new MusicStore.Models.SampleData());
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
